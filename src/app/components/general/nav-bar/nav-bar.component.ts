7e1a8c532e0bdaa2ff5c90efa01fe4b2a2ba42fb
DanielMP1995 <danielsantiagomeraparedes@hotmail.com.com>
2025-07-31 09:41:49 -0500

    pagina contactos y home

--- src/app/components/general/nav-bar/nav-bar.component.ts
+++ src/app/components/general/nav-bar/nav-bar.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
+  onUserClick() {
+    // Aquí puedes agregar la lógica para el botón de usuario
+    console.log('Usuario clickeó el botón de perfil/roles');
+    // Por ejemplo, abrir un modal o navegar a una página de perfil
+  }
 }
--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { ContactoInfoComponent } from "../../components/contactos/contacto-info/contacto-info.component";
 import { ContactoFormComponent } from "../../components/contactos/contacto-form/contacto-form.component";
 import { ContactoMapaComponent } from "../../components/contactos/contacto-mapa/contacto-mapa.component";
+import { NavBarComponent } from "../../components/general/nav-bar/nav-bar.component";
+import { FooterComponent } from "../../components/general/footer/footer.component";
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [ContactoInfoComponent, ContactoFormComponent, ContactoMapaComponent],
+  imports: [ContactoInfoComponent, ContactoFormComponent, ContactoMapaComponent, NavBarComponent, FooterComponent],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
