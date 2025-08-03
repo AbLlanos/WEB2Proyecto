2eb05e5eb85a61e28862a832df770bdb2ae986f4
DanielMP1995 <danielsantiagomeraparedes@gmail.com>
2025-08-03 12:24:23 -0500

    ultimos cambios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { NavBarComponent } from "../../components/general/nav-bar/nav-bar.compon
 import { CarruselComponent } from "../../components/home/carrusel/carrusel.component";
 import { FooterComponent } from "../../components/general/footer/footer.component";
 import { HeroComponent } from "../../components/home/hero/hero.component";
+import { MisionVisionComponent } from "../../components/mision-vision/mision-vision.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, CarruselComponent, FooterComponent, HeroComponent],
+  imports: [NavBarComponent, CarruselComponent, FooterComponent, HeroComponent, MisionVisionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
